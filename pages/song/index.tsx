bb9af5876f6dc1de4ea804d8870eff91516165a9
WyLToR <bekesi.patrik@gmail.com>
2024-02-05 19:39:21 +0100

    song page with player

---
+++ pages/song/index.tsx
@@ -0,0 +1,34 @@
+import {backendurl} from "@/src/constants/contants";
+import SongDetail from '@/components/song/song'
+import {useEffect, useState} from "react";
+
+export const getStaticProps = async () => {
+    const resp = await fetch(`${backendurl}/song`);
+    const data = await resp.json()
+    return {
+        props: {
+            song: data
+        }
+    }
+}
+export default function SongPage({song}: { song: Song[] }) {
+    const [isClient, setIsClient] = useState(false);
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+    return (
+        !isClient || (
+            <>
+                <main className="grid place-items-center">
+                    <section className="mx-auto grid grid-cols-1 sm:grid-cols-3 gap-4">
+                        {song.map((song: any) => (
+                            <div key={song.id}>
+                                <SongDetail song={song}/>
+                            </div>
+                        ))}
+                    </section>
+                </main>
+            </>
+        )
+    )
+}
