3156bb4cb827b3a24a84ac5cabe91cae90fe3e81
WyLToR <bekesi.patrik@gmail.com>
2024-02-05 19:39:45 +0100

    basic user/playlist pages

---
+++ pages/playlist/index.tsx
@@ -0,0 +1,7 @@
+export default function PlaylistPage(){
+    return(
+        <>
+            <h1 className="font-extralight accent-white">Playlist Page</h1>
+        </>
+    )
+}
---
+++ pages/user/index.tsx
@@ -0,0 +1,7 @@
+export default function UserPage(){
+    return(
+        <>
+            <h1 className="font-extralight accent-white">User Page</h1>
+        </>
+    )
+}
