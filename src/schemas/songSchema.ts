66a464fac1571c048b95391474c861796de06128
WyLToR <bekesi.patrik@gmail.com>
2024-02-06 12:15:26 +0100

    remove unnecessary rows from song schema

--- src/schemas/songSchema.ts
+++ src/schemas/songSchema.ts
@@ -4,22 +4,6 @@ const songSchema = Yup.object({
     title: Yup.string()
         .required('Title is required')
         .min(3, 'Min 3 character required'),
-    // songFile: Yup.mixed()
-    //     .required('File is required')
-    //     .test('is-audio', 'File must be an audio file', (value) => {
-    //         return isAudioFile(value);
-    //     }),
 })
-
-function isAudioFile(value: any) {
-    if (!value) {
-        return false;
-    }
-
-    const supportedFormats = ['mp3', 'wav', 'ogg'];
-    const fileExtension = value.split('.').pop();
-
-    return supportedFormats.includes(fileExtension.toLowerCase());
-}
 
 export default songSchema;
